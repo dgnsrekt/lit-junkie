3993833a954fc38ff54a295c19cefff45a7f16c0
dgnsrekt <dgnsrekt@pm.me>
2021-12-21 09:46:17 -0600

    chore: created basic concept for CLI menu/prompt.

---
+++ lit_junkie/views.py
@@ -0,0 +1,21 @@
+# - HEADERS
+# MAIN MENU -> BOOK SEARCH
+# | -> READING LIST MENU: show if file exists
+# BOOK SEARCH MENU -> SEARCH RESULTS MENU: if books found
+# | -> NOT FOUND MENU: if no books found
+# SEARCH RESULTS MENU -> READING LIST MENU: if book selected for storage
+# | -> MAIN MENU: if no book selected
+# NOT FOUND MENU: -> BOOK SEARCH if selected
+# | -> MAIN MENU: if selected
+# READING LIST MENU -> MAIN MENU
+
+# - DISPLAY ORDER
+# LOGO
+# HEADER
+# press (CTRL + C) to exit the application.
+# SELECTION
+# INPUT
+
+# - INPUT STYLES
+# SINGLE SELECT PROMPT [0, 1, 2]
+# SEARCH PROMPT text input
