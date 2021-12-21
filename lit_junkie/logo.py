285724be0285d82ba102c8a2b7cdbe631a4a2c65
dgnsrekt <dgnsrekt@pm.me>
2021-12-21 13:01:27 -0600

    feat: Added helper method to get the width of the logo.

--- lit_junkie/logo.py
+++ lit_junkie/logo.py
@@ -13,3 +13,15 @@ LOGO = """
     █░░ █ ▀█▀ ▄▄ ░░█ █░█ █▄░█ █▄▀ █ █▀▀
     █▄▄ █ ░█░ ░░ █▄█ █▄█ █░▀█ █░█ █ ██▄
 """
+
+
+def get_logo_width():
+    """Gets the logos width."""
+    width = 0
+    for row in LOGO.split("\n"):
+        if len(row) > width:
+            width = len(row)
+    return width
+
+
+LOGO_WIDTH = get_logo_width()
